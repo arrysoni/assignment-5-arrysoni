7e2a461b430c2b991cfc0f4b0702fe8f275ebbc8
Aarya Soni <137558851+arrysoni@users.noreply.github.com>
2024-12-13 11:50:42 -0500

    Update net.h

--- net.h
+++ net.h
@@ -6,7 +6,7 @@
 
 #define HEADER_LEN (sizeof(uint32_t) + sizeof(uint8_t))
 #define JBOD_SERVER "127.0.0.1"
-#define JBOD_PORT 3333
+#define JBOD_PORT 3000     // The port number needs to be changed everytime we check for a trace file 
 
 int jbod_client_operation(uint32_t op, uint8_t *block);
 bool jbod_connect(const char *ip, uint16_t port);
